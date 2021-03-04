68f0dd3fc69b3e7a50c34ca0041bc19f15642a52
M <ycliu@MacBook-Pro.attlocal.net>
2021-03-04 03:53:57 -0500

    Added homework of week 1

--- UCSD/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ UCSD/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -2,16 +2,38 @@
 #include <vector>
 #include <algorithm>
 
-int MaxPairwiseProduct(const std::vector<int>& numbers) {
-    int max_product = 0;
+long long MaxPairwiseProduct(const std::vector<int>& numbers) {
+    long long max_product = 0;
     int n = numbers.size();
 
-    for (int first = 0; first < n; ++first) {
-        for (int second = first + 1; second < n; ++second) {
-            max_product = std::max(max_product,
-                numbers[first] * numbers[second]);
+    if(n == 2) {
+        return (long long) numbers[0] * numbers[1];
+    }
+
+    int index_largest = 0; 
+    int largest = numbers[0];
+    for(int i = 1; i<n; ++i) {
+        if(numbers[i] > largest) {
+            largest = numbers[i];
+            index_largest = i;
         }
     }
+
+    int index_second_largest = 0; 
+    int second_largest = numbers[0];
+    if(index_largest == 0) {
+        int index_second_largest = 1; 
+        int second_largest = numbers[1];
+    }
+
+    for(int i = 1; i<n; ++i) {
+        if(numbers[i] > second_largest and i != index_largest) {
+            second_largest = numbers[i];
+            index_second_largest= i;
+        }
+    }
+    
+    max_product = (long long) numbers[index_largest] * numbers[index_second_largest];
 
     return max_product;
 }
@@ -24,6 +46,6 @@ int main() {
         std::cin >> numbers[i];
     }
 
-    std::cout << MaxPairwiseProduct(numbers); << "\n";
+    std::cout << MaxPairwiseProduct(numbers) << "\n";
     return 0;
 }
