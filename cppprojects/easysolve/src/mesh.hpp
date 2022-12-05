f1de67f4c6cfb444275d24c4b2c4184ab6ba4b57
M <mycliu10@gmail.com>
2022-12-04 16:11:27 -0800

    Change didx name

--- cppprojects/easysolve/src/mesh.hpp
+++ cppprojects/easysolve/src/mesh.hpp
@@ -38,29 +38,30 @@ public:
 class UniformMesh : public Mesh {
     double lx;
     double dx;
-    double dx2;
+    double didx;
+    double d2idx2;
 
 public:
     UniformMesh(shared_ptr<Index> index, shared_ptr<BoundaryConditionsSet> boundaryConditionsSet, double lx) 
         : Mesh(index, boundaryConditionsSet) {
         this->lx = lx;
         dx = lx / index->getNumCell(0);
-        dx2 = 1. / (dx * dx);
         x.resize(index, boundaryConditionsSet);
         for (auto gen = index->getAllZone(0); gen.isValid(); gen.next()) {
             int const i = gen.getCurrent();
             x.element({ i }) = dx * (i - index->getBegin(0));
         }
-        dx = 1. / dx;
+        didx = 1. / dx;
+        d2idx2 = didx * didx;
     }
 
     double getDidx(vector<int> & point) {
         (void) point;
-        return dx;
+        return didx;
     }
 
     double getD2idx2(vector<int> & point) const {
         (void) point;
-        return dx2;
+        return d2idx2;
     }
 };
